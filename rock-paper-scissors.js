3b40dfa3386d6398f9e0cef0dd6dc1736ce5d10f
rotas256 <97214237+rotas256@users.noreply.github.com>
2025-10-29 16:52:56 +0900

    Feat: Add function to get the computer choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+/*
+PROCESS GetComputerChoice
+    CREATE a random number
+    CALCULATE random integer from random number
+    CALCULATE remainder of a random number divide by 3
+    IF remainder = 0
+        RETURN "rock"
+    ELSE IF remainder = 1
+        RETURN "paper"
+    ELSE
+        RETURN "scissors"
+END
+*/
+function getComputerChoice(){
+    const maxNum = 65535;
+    let rand = Math.random();
+    let randInt = parseInt(rand*maxNum, 10);
+    
+    let remainder = randInt % 3;
+
+    if( remainder == 0 ) return "rock"
+    else if( remainder == 1) return "paper"
+    else return "scissors"
+}
