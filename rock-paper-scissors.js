ebeb220878bd050753d3673bd2afde623ecd0b6a
rotas256 <97214237+rotas256@users.noreply.github.com>
2025-11-06 17:03:24 +0900

    Feat: Add function to play entire game(5 rounds)

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -34,94 +34,129 @@ function getHumanChoice(){
     return humanChoice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
 /*
-PROCESS PlayRound
-    CONVERT humanChoice to lowercase
-    CASE humanChoice = rock :
-        CASE computerChoice = rock : 
-            PRINT "Draw!"
-        CASE computerChoice = paper :
-            PRINT "You lose! Paper beats Rock"
-            INCREMENT computerScore
-        CASE computerChoice = scissors :
-            PRINT "You win! Rock beats Scissors"
-            INCREMENT humanScore
-    CASE humanChoice = paper :
-        CASE computerChoice = rock :
-            PRINT "You win! Paper beats Rock"
-            INCREMENT humanScore
-        CASE computerChoice = paper :
-            PRINT "Draw!"
-        CASE computerChoice = scissors :
-            PRINT "You lose! Scissors beats Paper"
-            INCREMENT computerScore
-    CASE humanChoice = scissors :
-        CASE computerChoice = rock :
-            PRINT "You lose! Rock beats Scissors"
-            INCREMENT computerScore
-        CASE computerChoice = paper :
-            PRINT "You win! Paper beats Scissors"
-            INCREMENT humanScore
-        CASE computerChoice = scissors :
-            PRINT "Draw!"
+PROCESS PlayGame
+    INIT humanScore
+    INIT computerScore
+    FOR 5 rounds
+        INPUT humanChoice
+        SET computerChoice
+        CALL playRound(humanChoice, computerChoice)
+    ENDFOR
+    IF humanScore > computerScore THEN
+        PRINT "You win!"
+    ELSE IF humanScore < computerScore THEN
+        PRINT "You lose!"
+    ELSE
+        PRINT "Draw!"
 END
 */
-function playRound(humanChoice, computerChoice){
-    const unifiedHumanChoice = humanChoice.toLowerCase();
-    switch(unifiedHumanChoice){
-        case "rock":
-            switch(computerChoice){
-                case "rock":
-                    console.log("Draw!");
-                    break;
-                case "paper":
-                    console.log("You lose! Paper beats Rock");
-                    computerScore++;
-                    break;
-                case "scissors":
-                    console.log("You win! Rock beats Scissors");
-                    humanScore++;
-                    break;
-            }
-            break;
-        case "paper":
-            switch(computerChoice){
-                case "rock":
-                    console.log("You win! Paper beats Rock");
-                    humanScore++;
-                    break;
-                case "paper":
-                    console.log("Draw!");
-                    break;
-                case "scissors":
-                    console.log("You lose! Scissors beats Paper");
-                    computerScore++;
-                    break;
-            }
-            break;
-        case "scissors":
-            switch(computerChoice){
-                case "rock":
-                    console.log("You lose! Rock beats Scissors");
-                    computerScore++;
-                    break;
-                case "paper":
-                    console.log("You win! Scissors beats Paper");
-                    humanScore++;
-                    break;
-                case "scissors":
-                    console.log("Draw!");
-                    break;
-            }
-            break;
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    /*
+    PROCESS PlayRound
+        CONVERT humanChoice to lowercase
+        CASE humanChoice = rock :
+            CASE computerChoice = rock : 
+                PRINT "Draw!"
+            CASE computerChoice = paper :
+                PRINT "You lose! Paper beats Rock"
+                INCREMENT computerScore
+            CASE computerChoice = scissors :
+                PRINT "You win! Rock beats Scissors"
+                INCREMENT humanScore
+        CASE humanChoice = paper :
+            CASE computerChoice = rock :
+                PRINT "You win! Paper beats Rock"
+                INCREMENT humanScore
+            CASE computerChoice = paper :
+                PRINT "Draw!"
+            CASE computerChoice = scissors :
+                PRINT "You lose! Scissors beats Paper"
+                INCREMENT computerScore
+        CASE humanChoice = scissors :
+            CASE computerChoice = rock :
+                PRINT "You lose! Rock beats Scissors"
+                INCREMENT computerScore
+            CASE computerChoice = paper :
+                PRINT "You win! Paper beats Scissors"
+                INCREMENT humanScore
+            CASE computerChoice = scissors :
+                PRINT "Draw!"
+    END
+    */
+    function playRound(humanChoice, computerChoice){
+        const unifiedHumanChoice = humanChoice.toLowerCase();
+        switch(unifiedHumanChoice){
+            case "rock":
+                switch(computerChoice){
+                    case "rock":
+                        console.log("Draw!");
+                        break;
+                    case "paper":
+                        console.log("You lose! Paper beats Rock");
+                        computerScore++;
+                        break;
+                    case "scissors":
+                        console.log("You win! Rock beats Scissors");
+                        humanScore++;
+                        break;
+                }
+                break;
+            case "paper":
+                switch(computerChoice){
+                    case "rock":
+                        console.log("You win! Paper beats Rock");
+                        humanScore++;
+                        break;
+                    case "paper":
+                        console.log("Draw!");
+                        break;
+                    case "scissors":
+                        console.log("You lose! Scissors beats Paper");
+                        computerScore++;
+                        break;
+                }
+                break;
+            case "scissors":
+                switch(computerChoice){
+                    case "rock":
+                        console.log("You lose! Rock beats Scissors");
+                        computerScore++;
+                        break;
+                    case "paper":
+                        console.log("You win! Scissors beats Paper");
+                        humanScore++;
+                        break;
+                    case "scissors":
+                        console.log("Draw!");
+                        break;
+                }
+                break;
+        }
+    }
+    
+    const rounds = 5;
+    let humanSelection;
+    let computerSelection;
+    for( let i=0 ; i<rounds; i++){
+        humanSelection = getHumanChoice();
+        computerSelection = getComputerChoice();
+        console.log("COM:"+computerSelection);
+
+        playRound(humanSelection, computerSelection);
+    }
+    console.log("-----");
+    console.log("Score: [You:"+humanScore+"] [Com:"+computerScore+"]");
+    if(humanScore > computerScore){
+        console.log("You win!");
+    }else if(humanScore < computerScore){
+        console.log("You lose!");
+    }else{
+        console.log("Draw!");
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-console.log("COM:"+computerSelection);
-
-playRound(humanSelection, computerSelection);
+playGame();
